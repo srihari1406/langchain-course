010239b5f73f1250c916da42737b2e97692e67aa
Srihari Soundararajan <srihari@Sriharis-MacBook-Air.local>
2026-05-11 07:32:03 +0530

    web serach tool using langchain-tavily

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_groq import ChatGroq
-from tavily import TavilyClient
+# from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
-tavily = TavilyClient()
+# tavily = TavilyClient()
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    # return "Tokyo weather is sunny"
-    return tavily.search(query=query)
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for {query}")
+#     # return "Tokyo weather is sunny"
+#     return tavily.search(query=query)
 
 llm = ChatGroq(model="openai/gpt-oss-20b")
-tools = [search]
+# tools = [search]
+tools = [TavilySearch()]
 prompt = ChatPromptTemplate.from_messages([("human", "{input}"),MessagesPlaceholder(variable_name="agent_scratchpad")])
 agent = create_tool_calling_agent(llm=llm,tools=tools,prompt=prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
